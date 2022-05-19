cd7564fea6813d3b98b583b400d97bad1681d9d5
Mehran Falahati <mehran_falahati@yahoo.com>
2022-05-19 15:59:31 +1000

    form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create user_params
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
 end
