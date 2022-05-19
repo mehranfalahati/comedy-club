e690ae85c7668bacb3db104c01b4ca7c240fd71b
kristabel-wong <kristabel.wong@hotmail.com>
2022-05-19 14:18:26 +1000

    controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new user_params
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :first_name, :last_name)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.destroy_all
+
+u1 = User.create :first_name => 'Jane', :last_name => 'Doe', :email => 'janedoe@ga.co' 
+u2 = User.create :first_name => 'John', :last_name => 'Smith', :email => 'johnsmith@ga.co'
+u3 = User.create :first_name =>'Kris', :last_name => 'Wong', :email => 'kris@ga.co'
+u4 = User.create :first_name =>'Claire', :last_name => 'Williams', :email => 'claire@ga.co'
+u5 = User.create :first_name =>'Ben', :last_name => 'Miller', :email => 'ben@ga.co'
+u6 = User.create :first_name =>'Jasmin', :last_name => 'Yusuf', :email => 'jas@ga.co'
+u7 = User.create :first_name =>'Shaq', :last_name => 'gif-test', :email => 'shaq@ga.co'
+u8 = User.create :first_name => 'Default', :last_name => 'Default', :email => 'test@ga.co'
+
+puts "#{ User.count } users."
