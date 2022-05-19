c1ae8748d0478c7dfc86360dbf38ec475f3a4586
kristabel-wong <kristabel.wong@hotmail.com>
2022-05-19 14:22:24 +1000

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+    root :to => 'pages#home'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :users
 end
