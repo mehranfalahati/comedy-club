c6fd4daca8e221122ef84cc0f722666c99b4c067
Mehran Falahati <mehran_falahati@yahoo.com>
2022-05-20 11:37:29 +1000

    email is validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :email, :presence => true, :uniqueness => true
+    validates :email, :presence => true, :uniqueness => true, format: { with: /\A[a-zA-Z0-9]((?!\.\.)(?!\+\+)[\w\-+.])*[\w\-]@[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]*)+[a-zA-Z]\z/, message: "must be a valid email"}
     validates :first_name, format: { with: /\A[a-zA-Z]+\z/, message: "must contain letters only" }
     validates :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "must contain letters only" }
 end
